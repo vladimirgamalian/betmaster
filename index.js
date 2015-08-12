04ee806bae060453ed5fad8778b3313d1650bd89
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2015-08-12 18:39:10 +0700

    Add multiply leagues

--- index.js
+++ index.js
@@ -5,16 +5,139 @@ var phantom = require('x-ray-phantom');
 var requestModule = require('request');
 var cheerio = require('cheerio');
 var jar = requestModule.jar();
+var Q = require('q');
 
 var _ = require('lodash');
 var moment = require('moment-timezone');
 var bodyParser = require('body-parser');
 
-var serverTz = 'Asia/Bangkok';
+var betExplorerTzOffset = 2;    // GMT+2 (Moscow)
 var moscowTz = 'Europe/Moscow';
 var betExplorerUri = 'http://www.betexplorer.com';
-var prevSeasonUrl = 'http://www.betexplorer.com/soccer/england/premier-league-2014-2015/results/';
-var curSeasonUrl = 'http://www.betexplorer.com/soccer/england/premier-league/fixtures/';
+//var prevSeasonUrl = 'http://www.betexplorer.com/soccer/england/premier-league-2014-2015/results/';
+//var curSeasonUrl = 'http://www.betexplorer.com/soccer/england/premier-league/fixtures/';
+
+// Convert DD.MM.YYYY to YYYY-MM-DD
+function convertDate(s) {
+    return s.substr(6, 4) + '-' + s.substr(3, 2) + '-' + s.substr(0, 2);
+}
+
+// Convert DD.MM.YYYY hh:mm to YYYY-MM-DDThh:mm+03:00
+function convertDateTime(s) {
+    return s.substr(6, 4) + '-' + s.substr(3, 2) + '-' + s.substr(0, 2) + 'T' + s.substr(11, 2) + ':' + s.substr(14, 2) + '+03:00';
+}
+
+function tzOffsetToStr(v) {
+    if (v < 0) {
+        return v.toString();
+    }
+    return '+' + v.toString();
+}
+
+function betExplorerScrapResults(uri, minDate) {
+    var deferred = Q.defer();
+
+    jar.setCookie(requestModule.cookie('my_timezone=' + tzOffsetToStr(betExplorerTzOffset)), betExplorerUri);
+    requestModule({uri: uri, jar: jar}, function (error, resp, html) {
+        var currentRound = 0,
+            $,
+            siteTzSelector,
+            realTzOffset,
+            result = [];
+
+        if (!error && resp.statusCode == 200) {
+
+            $ = cheerio.load(html);
+            siteTzSelector = $('#tz-switch a').text();
+            realTzOffset = parseInt(siteTzSelector.match(/[^+-]*(.\d{1,2})/)[1], 10);
+
+            if (realTzOffset != betExplorerTzOffset) {
+                console.log('WARNING', betExplorerTzOffset, realTzOffset);
+            }
+
+            $('table.league-results tr').each(function (i, elem) {
+                var round = $(this).find('th.first-cell').text(),
+                    match = $(this).find('td.first-cell').text(),
+                    date = $(this).find('td.last-cell').text();
+                if (round) {
+                    currentRound = parseInt(round, 10);
+                } else {
+                    result.push({
+                        match: match,
+                        date: moment.tz(convertDate(date), moscowTz),
+                        round: currentRound
+                    });
+                }
+            });
+
+            result = _.filter(result, function (i) {
+                return i.date >= minDate;
+            });
+
+            deferred.resolve(result)
+        } else {
+            deferred.reject(error);
+        }
+    });
+
+    return deferred.promise;
+}
+
+function betExplorerScrapFixtures(uri, requestedDate) {
+    var deferred = Q.defer();
+
+    jar.setCookie(requestModule.cookie('my_timezone=' + tzOffsetToStr(betExplorerTzOffset)), betExplorerUri);
+    requestModule({uri: uri, jar: jar}, function (error, resp, html) {
+        var currentRound = 0,
+            $,
+            siteTzSelector,
+            realTzOffset,
+            result = [],
+            currentDateTime;
+
+        if (!error && resp.statusCode == 200) {
+
+            $ = cheerio.load(html);
+            siteTzSelector = $('#tz-switch a').text();
+            realTzOffset = parseInt(siteTzSelector.match(/[^+-]*(.\d{1,2})/)[1], 10);
+
+            if (realTzOffset != betExplorerTzOffset) {
+                console.log('WARNING', betExplorerTzOffset, realTzOffset);
+            }
+
+            $('table.league-fixtures tr').each(function (i, elem) {
+                var round = $(this).find('th.first-cell').text(),
+                    matchNode = $(this).find('td.nobr'),
+                    match = matchNode.text(),
+                    matchLink = $(matchNode).find('a').attr('href'),
+                    date = $(this).find('td.first-cell').text();
+                if (round) {
+                    currentRound = parseInt(round, 10);
+                } else {
+                    if (date.length >= 10) {
+                        currentDateTime = moment.tz(convertDateTime(date), moscowTz);
+                    }
+                    result.push({
+                        match: match,
+                        matchLink: betExplorerUri + matchLink,
+                        date: currentDateTime,
+                        round: currentRound
+                    });
+                }
+            });
+
+            result = _.filter(result, function (i) {
+                return i.date.startOf('day').isSame(requestedDate);
+            });
+
+            deferred.resolve(result)
+        } else {
+            deferred.reject(error);
+        }
+    });
+
+    return deferred.promise;
+}
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -29,35 +152,35 @@ app.get('/', function(request, response) {
 app.post('/api/data', function(request, response) {
     var x = Xray(),
         result = {},
-        tzOffset = 3,   // Moscow GMT+3
-        requestedDate = moment.tz(request.body.date, moscowTz);
+        tzOffset = betExplorerTzOffset,
+        requestedDate = moment.tz(request.body.date, moscowTz),
+        reqResults = request.body.results,
+        reqFixtures = request.body.fixtures,
+        reqResultsFrom = request.body.resultsFrom;
 
-    //console.log('requestedDate', requestedDate.format());
+    console.log('requestedDate', requestedDate.format());
+    console.log('reqResults', reqResults);
+    console.log('reqFixtures', reqFixtures);
+    console.log('reqResultsFrom', reqResultsFrom);
 
-    // Convert DD.MM.YYYY to YYYY-MM-DD
-    function convertDate(s) {
-        return s.substr(6, 4) + '-' + s.substr(3, 2) + '-' + s.substr(0, 2);
-    }
-
-    // Convert DD.MM.YYYY hh:mm to YYYY-MM-DDThh:mm+03:00
-    function convertDateTime(s) {
-        return s.substr(6, 4) + '-' + s.substr(3, 2) + '-' + s.substr(0, 2) + 'T' + s.substr(11, 2) + ':' + s.substr(14, 2) + '+03:00';
-    }
-
-    function tzOffsetToStr(v) {
-        if (v < 0) {
-            return v.toString();
-        }
-        return '+' + v.toString();
-    }
+    //betExplorerScrapResults(prevSeasonUrl, moment.tz(convertDate('25.04.2015'), moscowTz))
+    //    .then(function (prevSeason) {
+    //
+    //    })
+    //    .then()
+    //    .catch(function (err) {
+    //        result.error = err;
+    //        response.json(result);
+    //    });
 
     jar.setCookie(requestModule.cookie('my_timezone=' + tzOffsetToStr(tzOffset)), betExplorerUri);
-    requestModule({uri: prevSeasonUrl, jar: jar}, function (error, resp, html) {
+    requestModule({uri: reqResults, jar: jar}, function (error, resp, html) {
         var prevSeasonMatches = [],
             currentRound = 0,
-            maxRound,
+            //maxRound,
             minDate;
 
+        //TODO: resp is undefined when incorrect url like /soccer/fixtures/ (without domain).
         if (!error && resp.statusCode == 200) {
             var $ = cheerio.load(html),
                 siteTzSelector = $('#tz-switch a').text(),
@@ -84,12 +207,14 @@ app.post('/api/data', function(request, response) {
             });
 
             // Get highest round number.
-            maxRound = _.max(prevSeasonMatches, 'round').round;
+            //maxRound = _.max(prevSeasonMatches, 'round').round;
+            //
+            //// Get min date.
+            //minDate = _.min(_.filter(prevSeasonMatches, function(i) {
+            //    return i.round > maxRound - 5;
+            //}), 'date').date;
 
-            // Get min date.
-            minDate = _.min(_.filter(prevSeasonMatches, function(i) {
-                return i.round > maxRound - 5;
-            }), 'date').date;
+            minDate = moment.tz(convertDate(reqResultsFrom), moscowTz);
 
             // Get matches after min date.
             prevSeasonMatches = _.filter(prevSeasonMatches, function (i) {
@@ -97,7 +222,7 @@ app.post('/api/data', function(request, response) {
             });
 
             // Now get current season.
-            requestModule({uri: curSeasonUrl, jar: jar}, function (error, resp, html) {
+            requestModule({uri: reqFixtures, jar: jar}, function (error, resp, html) {
                 var currSeasonMatches = [],
                     currentDateTime;
 
--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,109 @@ angular.module('betmaster', ['ui.bootstrap'])
         var that = this;
         that.loading = false;
         that.dt = new Date(moment().tz('Europe/Moscow').startOf('day').format('YYYY-MM-DD'));
+        that.data = [];
+
+        that.leaguesPrefix = 'http://www.betexplorer.com';
+        that.leagues = [
+            {
+                results: '/soccer/england/premier-league-2014-2015/results/',
+                fixtures: '/soccer/england/premier-league/fixtures/',
+                name: 'England Premier League',
+                resultsFrom: '25.04.2015'
+            },
+            {
+                results: '/soccer/england/championship-2014-2015/results/',
+                fixtures: '/soccer/england/championship/fixtures/',
+                name: 'England Championship',
+                resultsFrom: '10.04.2015'
+            },
+            {
+                results : '/soccer/france/ligue-1-2014-2015/results/',
+                fixtures : '/soccer/france/ligue-1/fixtures/',
+                name : 'France Ligue 1',
+                resultsFrom : '17.04.2015'
+            },
+            {
+                results : '/soccer/france/ligue-2-2014-2015/results/',
+                fixtures : '/soccer/france/ligue-2/fixtures/',
+                name : 'France Ligue 2',
+                resultsFrom : '24.04.2015'
+            },
+            {
+                results : '/soccer/france/national-2014-2015/results/',
+                fixtures : '/soccer/france/national/fixtures/',
+                name : 'France National',
+                resultsFrom : '17.04.2015'
+            },
+            {
+                results : '/soccer/italy/serie-a-2014-2015/results/',
+                fixtures : '/soccer/italy/serie-a/fixtures/',
+                name : 'Italy Serie-A',
+                resultsFrom : '28.04.2015'
+            },
+            {
+                results : '/soccer/spain/primera-division-2014-2015/results/',
+                fixtures : '/soccer/spain/primera-division/fixtures/',
+                name : 'Spain Primera Division',
+                resultsFrom : '24.04.2015'
+            },
+            {
+                results : '/soccer/spain/segunda-division-2014-2015/results/',
+                fixtures : '/soccer/spain/segunda-division/fixtures/',
+                name : 'Spain Segunda Division',
+                resultsFrom : '09.05.2015'
+            },
+            {
+                results : '/soccer/germany/bundesliga-2014-2015/results/',
+                fixtures : '/soccer/germany/bundesliga/fixtures/',
+                name : 'Germany Bundesliga',
+                resultsFrom : '24.04.2015'
+            },
+            {
+                results : '/soccer/germany/2-bundesliga-2014-2015/results/',
+                fixtures : '/soccer/germany/2-bundesliga/fixtures/',
+                name : 'Germany 2 Bundesliga',
+                resultsFrom : '24.04.2015'
+            },
+            {
+                results : '/soccer/germany/3-liga-2014-2015/results/',
+                fixtures : '/soccer/germany/3-liga/fixtures/',
+                name : 'Germany 3 Liga',
+                resultsFrom : '24.04.2015'
+            }
+        ];
+
+        that.fetchLeague = function (leagueIndex) {
+            var league = that.leagues[leagueIndex];
+            if (league) {
+                that.message = 'Обработка ' + league.name;
+
+                $http({
+                    url: '/api/data',
+                    method: "POST",
+                    data: {
+                        'date': moment(that.dt).format('YYYY-MM-DD'),
+                        'results': that.leaguesPrefix + league.results,
+                        'fixtures': that.leaguesPrefix + league.fixtures,
+                        'resultsFrom': league.resultsFrom
+                    }
+                }).then(function(response) {
+                    that.data = that.data.concat(response.data);
+                }).catch(function () {
+                    that.message = 'Ошибка';
+                }).finally(function () {
+                    that.fetchLeague(leagueIndex + 1);
+                });
+
+            } else {
+                that.loading = false;
+                that.message = 'Запрос завершен';
+            }
+        };
 
         that.fetchData = function () {
-            that.data = null;
+            that.data = [];
             that.loading = true;
-            $http({
-                url: '/api/data',
-                method: "POST",
-                data: {'date': moment(that.dt).format('YYYY-MM-DD')}
-            }).then(function(response) {
-                that.data = response.data;
-            }).finally(function () {
-                that.loading = false;
-            });
+            that.fetchLeague(0);
         }
     });
